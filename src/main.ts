37c6d523f2937deaed3a567f21af9270f6cca5d7
Ntholi Nkhatho <ntholi@live.com>
2024-10-20 21:00:06 +0200

    add cli commands for creating table

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,46 @@
 import { Command } from 'commander';
 import chalk from 'chalk';
 
+interface Property {
+  name: string;
+  type: string;
+}
+
 const program = new Command();
 
 program
   .version('0.0.1-alpha.0')
-  .description('Adease CLI')
-  .option('-n, --name <name>', 'Your name')
-  .option('-g, --greet', 'Greet the user')
-  .parse(process.argv);
+  .description('Adease CLI tool for creating tables');
 
-const options = program.opts();
+program
+  .command('create')
+  .description('Create a new table with specified properties')
+  .argument('<tableName>', 'Name of the table to create')
+  .argument('[properties...]', 'Properties in the format propertyName:dataType')
+  .action((tableName: string, properties: string[]) => {
+    console.log(chalk.green('Creating table'), chalk.yellow(tableName));
 
-if (options.greet) {
-  const name = options.name || 'User';
-  console.log(chalk.green(`Hello, ${name}! Welcome to Adease CLI.`));
-} else {
-  console.log(chalk.yellow('Use --help to see available commands'));
-}
+    const parsedProperties: Property[] = properties
+      .filter((prop) => prop.includes(':'))
+      .map((prop) => {
+        const [name, type] = prop.split(':');
+        if (!name || !type) {
+          console.log(chalk.red(`Invalid property format: ${prop}. Skipping.`));
+          return null;
+        }
+        return { name, type };
+      })
+      .filter((prop): prop is Property => prop !== null);
+
+    if (parsedProperties.length > 0) {
+      console.log(chalk.blue('Properties:'));
+      parsedProperties.forEach((prop) => {
+        console.log(chalk.yellow(`  ${prop.name}: ${prop.type}`));
+      });
+    } else {
+      console.log(chalk.yellow('No valid properties provided.'));
+    }
+    console.log(chalk.green('Table created successfully!'));
+  });
+
+program.parse(process.argv);
