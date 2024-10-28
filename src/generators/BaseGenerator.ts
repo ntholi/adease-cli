ac4e3da065e0038d387add7c07ce34c6eb5984e9
Ntholi Nkhatho <ntholi@live.com>
2024-10-28 19:56:37 +0200

    create method to generate file in BaseGenerator

--- src/generators/BaseGenerator.ts
+++ src/generators/BaseGenerator.ts
@@ -1,6 +1,28 @@
+import { fileURLToPath } from 'url';
 import Answers from '../types/Answers';
+import { readConfig } from '../utils/config';
+import path from 'path';
+import ejs from 'ejs';
+import fs from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export abstract class BaseGenerator {
-  constructor(protected readonly answers: Answers) {}
+  protected config: { baseDir: string; adminDir: string };
+
+  constructor(protected readonly answers: Answers) {
+    this.config = readConfig();
+  }
+
+  protected async generateFile(
+    template: string,
+    output: string
+  ): Promise<void> {
+    const temp = await fs.readFile(path.join(__dirname, template), 'utf8');
+    const compiled = ejs.compile(temp);
+    await fs.writeFile(path.join(process.cwd(), output), compiled({}));
+  }
+
   abstract generate(): Promise<void>;
 }
--- src/generators/IndexPage/index.ts
+++ src/generators/IndexPage/index.ts
@@ -1,12 +1,5 @@
-import { BaseGenerator } from '../BaseGenerator';
-import ejs from 'ejs';
-import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import Answers from '../../types/Answers';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { BaseGenerator } from '../BaseGenerator';
 
 class IndexPageGenerator extends BaseGenerator {
   constructor(answers: Answers) {
@@ -14,13 +7,7 @@ class IndexPageGenerator extends BaseGenerator {
   }
 
   async generate(): Promise<void> {
-    const template = await fs.readFile(
-      path.join(__dirname, 'template.ejs'),
-      'utf8'
-    );
-    const compiled = ejs.compile(template);
-    const output = compiled({});
-    await fs.writeFile(path.join(process.cwd(), 'index.ts'), output);
+    await this.generateFile('template.ejs', 'index.ts');
   }
 }
 
