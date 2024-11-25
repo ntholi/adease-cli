7ec7ed85f72169e46944ac214954fe2d84b57d90
Ntholi Nkhatho <ntholi@live.com>
2024-11-25 14:53:08 +0200

    add more components

--- src/generators/Components/index.ts
+++ src/generators/Components/index.ts
@@ -8,12 +8,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const components = [
-  'SearchField',
-  'Pagination',
+  'DeleteButton',
+  'DetailsView',
+  'DetailsViewBody',
+  'DetailsViewHeader',
+  'FieldView',
+  'Form',
+  'FormHeader',
   'ListItem',
   'ListLayout',
-  'Shell',
   'NothingSelected',
+  'Pagination',
+  'SearchField',
+  'Shell',
 ];
 
 class ComponentsGenerator {
