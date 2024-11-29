bb68d1d124f80ae34b791aeb3923eb5c6bcd9e33
Ntholi Nkhatho <ntholi@live.com>
2024-11-29 21:44:07 +0200

    refactor create command to improve table creation logic and user prompts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import chalk from 'chalk';
 import { Command } from 'commander';
-import { Field } from './types/Field';
-import Answers from './types/Answers';
-import { generateAll } from './commands/create';
-import inquirer from 'inquirer';
+import { createCommand } from './commands/create';
 import { init } from './commands/init';
 
 const program = new Command();
@@ -18,74 +14,7 @@ program
   .argument('<tableName>', 'Name of the table to create')
   .argument('[fields...]', 'Fields in the format fieldName:fieldType')
   .option('-y, --yes', 'Skip all prompts and use default values')
-  .action(
-    async (
-      tableName: string,
-      rawFields: string[],
-      options: { yes?: boolean }
-    ) => {
-      console.log(chalk.green(`Creating table: ${tableName}`));
-      const fields = rawFields
-        .filter((field) => field.includes(':'))
-        .map((field) => {
-          const [name, type] = field.split(':');
-          if (!name || !type) {
-            console.log(chalk.red(`Invalid field format: ${field}. Skipping.`));
-            return null;
-          }
-          return { name, type };
-        })
-        .filter((field): field is Field => field !== null);
-
-      if (fields.length > 0) {
-        console.log(chalk.blue('Fields:'));
-        fields.forEach((field) => {
-          console.log(chalk.yellow(`  ${field.name}: ${field.type}`));
-        });
-      } else {
-        console.log(chalk.yellow('No valid fields provided.'));
-        return;
-      }
-
-      let answers: Answers;
-      if (options.yes) {
-        answers = {
-          database: 'drizzle',
-          serviceFile: true,
-          apiRoutes: true,
-        };
-      } else {
-        console.log('--------------------------------');
-        answers = await inquirer.prompt([
-          {
-            type: 'list',
-            name: 'database',
-            message: 'Which database adapter would you like to use?',
-            choices: [
-              { name: 'Drizzle', value: 'drizzle' },
-              { name: 'Prisma', value: 'prisma' },
-              { name: 'Firebase', value: 'firebase' },
-            ],
-            default: 'drizzle',
-          },
-          {
-            type: 'confirm',
-            name: 'serviceFile',
-            message: 'Generate service file?',
-            default: true,
-          },
-          {
-            type: 'confirm',
-            name: 'apiRoutes',
-            message: 'Generate API route handlers?',
-            default: true,
-          },
-        ]);
-      }
-
-      await generateAll(tableName, fields, answers);
-    }
-  );
+  .action(createCommand);
 
 program
   .command('init')
